87be110b61407b37f963f5a4bbd858d879a8d224
CleomarJCosta <cleomarcosta20@gmail.com>
2021-09-28 21:05:00 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ const express = require("express");
 const routes = express.Router();
 const HamburgerController = require("./app/controller/HamburgerController");
 
-// routes.get("/", function(req, res) {
-//     return res.send("Minha primeira rota!");
-//   });
+routes.get("/", function(req, res) {
+    return res.send("Minha primeira rota!");
+  });
 
 routes.get("/hamburger", HamburgerController.index);
+
+
 
 routes.post("/hamburger", HamburgerController.store);
 
